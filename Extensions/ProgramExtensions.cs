4bde4ebaf3af5a742dbe4d15225304e58fd819de
v.cernetchi <v.cernetchi@outlook.com>
2021-04-24 23:18:22 +0300

    add migrations on appStartup

---
+++ Extensions/ProgramExtensions.cs
@@ -0,0 +1,34 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OwlApp.Data;
+
+namespace OwlApp.Extensions
+{
+    public static class ProgramExtensions
+    {
+        public static IHost MigrateDatabase<T>(this IHost webHost) where T : OwlAppContext
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var db = services.GetRequiredService<T>();
+                    db.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating the database.");
+                }
+            }
+
+            return webHost;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using OwlApp.Data;
+using OwlApp.Extensions;
 
 namespace OwlApp
 {
@@ -13,7 +15,7 @@ namespace OwlApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().MigrateDatabase<OwlAppContext>().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
